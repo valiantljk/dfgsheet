052d7cc11ccbfc43700b32985293b3560a5372a5
Jialin Liu <valiantljk@gmail.com>
2017-07-24 17:19:08 -0700

    add dfgsheet

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'pysheet',
-  packages = ['pysheet'], # this must be the same as the name above
+  name = 'dfgsheet',
+  packages = ['dfgsheet'], # this must be the same as the name above
   version = '0.1',
   description = 'google sheet to dataframe',
   author = 'Jialin Liu',
   author_email = 'valiantljk@gmail.com',
-  url = 'https://github.com/valiantljk/pysheet.git', # use the URL to the github repo
-  download_url = 'https://github.com/valiantljk/pysheet/archive/0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/valiantljk/dfgsheet.git', # use the URL to the github repo
+  download_url = 'https://github.com/valiantljk/dfgsheet/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['dataframe', 'python', 'sheet'], # arbitrary keywords
   classifiers = [],
 )
